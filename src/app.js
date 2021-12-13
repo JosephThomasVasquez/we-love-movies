b2d265275e4c864fe536ec5ae3cfb09c1798749b
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-12-12 17:42:05 -0800

    Setup migrations and updated db migrations

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+ap.use(cors());
 
 module.exports = app;
